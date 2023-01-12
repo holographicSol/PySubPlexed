f4976bd5cc93e310c1260b4a7b7c012a60969f79
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2023-01-12 15:22:45 +0000

    Add files via upload

--- examples/example_5.py
+++ examples/example_5.py
@@ -13,18 +13,22 @@ def PySubPlexed(_data):
 print('Starting Program X: Using PySubPlexed to compute...')
 
 data = []
-for i in range(0, 8):
+for i in range(1, 65):
     _str = '10**' + str(i)
     data.append(_str)
 print('Data:  ', data)
 
-chunks = pysubplexed.chunk_data(data, 8)
+chunks = pysubplexed.chunk_data(data, 2)
 print('Chunks:', chunks)
 
 print('')
 results = []
 for chunk in chunks:
+    print('processing chunk:', chunk)
     result = PySubPlexed(chunk)
     results.append(result)
+    print('chunk result:    ', result)
+
 print('Items in results:', sum(len(chunk) for result in results))
-print('Results:', results)
+for _ in results:
+    print(_)
