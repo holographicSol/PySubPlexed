e53c21f323e05b59c543fd2c2eb5681bfad0e6d2
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2023-01-12 16:17:31 +0000

    Add files via upload

--- examples/example_7.py
+++ examples/example_7.py
@@ -1,6 +1,13 @@
 """ Written by Benjamin Jack Cullen
 Intention: Example program using PySubPlexed. Summary. Use potentially all CPU cores easily
 with PySubPlexed in one line.
+
+Things to remember:
+    1. Setting Daemon Count can be dangerous so stay within the logical limits of your hardware.
+       4 Cores, 8 threads = Set a max of 8 for n_thread (like below).
+    2. Restrain the daemons. Use only the daemon you need by setting restrained=True/False accordingly.
+    3. Consider eval() when passing lists/chunks of data to PyPortPlexed because eval() is extremely powerful.
+
 """
 import pysubplexed
 
