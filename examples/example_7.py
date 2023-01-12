264f039886b8079310ccafbdb653666e338bd092
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2023-01-12 16:11:01 +0000

    Add files via upload

---
+++ examples/example_7.py
@@ -0,0 +1,9 @@
+""" Written by Benjamin Jack Cullen
+Intention: Example program using PySubPlexed. Summary. Use potentially all cores easily with PySubPlexed in one line.
+"""
+import pysubplexed
+
+print('Starting Program X: Using PySubPlexed to compute...')
+
+print('Results:', len(pysubplexed.spawn(int(8), _data=['1024**100000', '1024**100000', '1024**100000', '1024**100000',
+                                                       '1024**100000', '1024**100000', '1024**100000', '1024**100000'])))
