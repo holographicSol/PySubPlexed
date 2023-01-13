60ceb0b0997c652989af5e06569b7b502abde90d
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2023-01-13 10:20:43 +0000

    Add files via upload

--- examples/example_10.py
+++ examples/example_10.py
@@ -1,17 +1,28 @@
 """ Written by Benjamin Jack Cullen
-Intention: Example program using PySubPlexed.
-Summary: Use potentially all CPU cores easily with PySubPlexed in one line.
-
-Things to remember:
-    1. Setting Daemon Count can be dangerous so stay within the logical limits of your hardware.
-       4 Cores, <=8 threads = Set a max of 8 for n_thread (like below).
-    2. Restrain the daemons. Use only the daemon you need by setting restrained=True/False accordingly.
-    3. Consider eval() when passing lists/chunks of data to PySubPlexed because eval() is extremely powerful.
-
-"""
+Intention: Testing."""
 import pysubplexed
+
+
+def PySubPlexed(_data):
+    """ Provide something for PySubPlexed to compute... In one line.
+
+    In this example let's return sorted un-tagged data and then un-chunk for a clean one to one list alignment
+    between expressions in and results out.
+    """
+
+    return pysubplexed.spawn(int(len(_data)), _data, restrained=False, tag=False, sort=True)
+
 
 print('Starting Program X: Using PySubPlexed to compute...')
 
-print('Results:', len(pysubplexed.spawn(int(8), _data=['1024**500000', '1024**500000', '1024**500000', '1024**100000',
-                                                       '1024**500000', '1024**500000', '1024**500000', '1024**100000'])))
+data = ['1+1', '2+1', '3+1', '4+1']
+chunks = pysubplexed.chunk_data(data, 2)
+
+""" Feed the chunks into PySubPlexed one by one """
+results = []
+for chunk in chunks:
+    result = PySubPlexed(chunk)
+    results.append(result)
+
+""" The data can then be un-chunked if desired """
+print(pysubplexed.unchunk_data(data=results))
