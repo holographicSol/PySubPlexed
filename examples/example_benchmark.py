7009253b567529e2ab85eddb3e11321ba5ef081a
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2023-01-13 15:32:32 +0000

    Add files via upload

--- examples/example_benchmark.py
+++ examples/example_benchmark.py
@@ -1,14 +1,18 @@
 import time
 import pysubplexed
 
+evalutate_this = '1024**100000000'
+
 
 def PySubPlexed(_data):
     return pysubplexed.spawn(int(len(_data)), _data, restrained=False, tag=False, sort=True)
 
 
 def BenchPySubPlexed():
-    """ Provide a sufficient workload to PySubPlexed to make it worth it. """
-    data = ['1024**100000000', '1024**100000000', '1024**100000000', '1024**100000000']
+    """ Provide a sufficient workload to PySubPlexed to make it worth it.
+    Try adding more zero's to evaluate_this (one at a time).
+    """
+    data = [evalutate_this, evalutate_this, evalutate_this, evalutate_this]
 
     t0 = time.perf_counter()
     chunk = pysubplexed.chunk_data(data, 4)
@@ -19,13 +23,13 @@ def BenchPySubPlexed():
 def BenchProceduralPy():
     t0 = time.perf_counter()
     results = []
-    x = eval('1024**100000000')
+    x = eval(evalutate_this)
     results.append(x)
-    y = eval('1024**100000000')
+    y = eval(evalutate_this)
     results.append(y)
-    z = eval('1024**100000000')
+    z = eval(evalutate_this)
     results.append(z)
-    a = eval('1024**100000000')
+    a = eval(evalutate_this)
     results.append(a)
     print('Time taken (Procedural Py):', time.perf_counter() - t0)
 
