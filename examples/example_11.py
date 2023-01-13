e0b806b8a067ffcea961f0148eca0a924512da86
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2023-01-13 11:12:45 +0000

    Add files via upload

--- examples/example_11.py
+++ examples/example_11.py
@@ -10,7 +10,7 @@ def PySubPlexed(_data):
     Observe I/O with small sample sizes when providing PySubPlexed with expressions
     and data combinations to test if your required expression and data will always
     provide the correct results and data structure.
-    Sampling is even more important if your expression are variable and or with
+    Sampling is even more important if your expression(s) are variable and or with
     truly variable data.
     You can say, okay I will always have these kinds of expression(s) with this kind
     of data so lets sample so many cycles and test with a program to ensure everything
