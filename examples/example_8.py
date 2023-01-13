66a7393f929a6195a2dd50cc235528c6fb489ea5
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2023-01-13 10:05:53 +0000

    Add files via upload

---
+++ examples/example_8.py
@@ -0,0 +1,31 @@
+""" Written by Benjamin Jack Cullen
+Intention: Testing."""
+import pysubplexed
+
+
+def PySubPlexed(_data):
+    """ Provide something for PySubPlexed to compute... In one line.
+
+    Example return un-tagged results 'unsorted' while still using internal tagging for order.
+    """
+
+    return pysubplexed.spawn(int(len(_data)), _data, restrained=False, tag=False, sort=False)
+
+
+print('Starting Program X: Using PySubPlexed to compute...')
+
+data = ['1+1', '2+1', '3+1', '4+1']
+chunks = pysubplexed.chunk_data(data, 2)
+
+""" Feed the chunks into PySubPlexed one by one """
+results = []
+i_results = 0
+for chunk in chunks:
+    result = PySubPlexed(chunk)
+    results.append(result)
+    i_results += int(len(result))
+
+print('Items in results:', i_results)
+
+""" The data can then be un-chunked if desired """
+print(pysubplexed.unchunk_data(data=results))
