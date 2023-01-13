bda909de25460618e7ab8bc2d481e2de0273ee45
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2023-01-13 23:27:44 +0000

    Add files via upload

--- examples/pysubplexed.py
+++ examples/pysubplexed.py
@@ -6,81 +6,101 @@ Summary: PySubPlex.
 import subprocess
 from threading import Thread
 
+""" Setup subprocess startupinfo argument to be daemonic """
 info = subprocess.STARTUPINFO()
 info.dwFlags = 1
 info.wShowWindow = 0
-r = []
-p = []
 
-
-def dec(s):
-    return str(s.decode("utf8").strip())
+res = []
+procs = []
 
 
 def results(n=str):
-    global p, r
-    co = []
+    """ Wait on n threads for results to come back in from real processes. """
+
+    global procs, res
+    cmd_output = []
     n = int(n)
     while True:
-        o = p[n].stdout.readline()
-        if o == '' and p[n].poll() is not None:
+        output = procs[n].stdout.readline()
+        if output == '' and procs[n].poll() is not None:
             break
-        if o:
-            co.append(dec(o))
+        if output:
+            cmd_output.append(str(output.decode("utf-8").strip()))
         else:
             break
-    if co:
-        r.append(co)
+    rc = procs[n].poll()
+    if cmd_output:
+        res.append(cmd_output)
 
 
-def spawn(nt, _d, restrained=False, tag=True, sort=True):
-    global p, r
-    p = []
-    r = []
+def spawn(n_thread, _data, restrained=False, tag=True, sort=True):
+    """ Starts n process(s) each with their ID and then spawns n threads to wait for the results come back in. """
+
+    global procs, res
+    procs = []
+    res = []
+
+    """ Use specified daemon """
     if restrained is False:
-        d = 'python ./pysubplexed_daemon.py '
+        PySubPlexCommand = 'python ./pysubplexed_daemon.py '
     elif restrained is True:
-        d = 'python ./pysubplexed_daemon_restrained.py '
-    com = []
-    for n in range(0, nt):
-        c = d + str(n) + ' ' + str(_d[n])
-        com.append(c)
-        x = subprocess.Popen(c, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-        p.append(x)
-        t = Thread(target=results, args=str(n))
-        t.start()
-    while len(r) < nt:
+        PySubPlexCommand = 'python ./pysubplexed_daemon_restrained.py '
+
+    """ Spawn and instruct daemons """
+    commands = []
+    for n in range(0, n_thread):
+        cmd = PySubPlexCommand + str(n) + ' ' + str(_data[n])
+        commands.append(cmd)
+    procs = [subprocess.Popen(i, stdout=subprocess.PIPE, stderr=subprocess.STDOUT) for i in commands]
+
+    """ Spawn threads and wait for results back from daemons """
+    for n in range(0, n_thread):
+        thread = Thread(target=results, args=str(n))
+        thread.start()
+    while len(res) < n_thread:
         pass
+
+    """ Sort the results by IDs and return (getting updated, return res as it is) """
     if sort is True:
-        r = sorted(r, key=lambda x: x[0])
-    z = []
+        res = sorted(res, key=lambda x: x[0])
+
+    """ Data structure return tagged/un-tagged results """
+    multiplexed_results = []
     if tag is True:
-        for x in r:
-            a = str(*x)
-            i = str(a).find(' ')
-            b = str(a)[:i]
-            c = str(a)[i+1:]
-            z.append([b, c])
+        for r in res:
+            a = str(*r)
+            idx = str(a).find(' ')
+            b = str(a)[:idx]
+            c = str(a)[idx+1:]
+            multiplexed_results.append([b, c])
     else:
-        for x in r:
-            a = str(*x)
-            i = str(a).find(' ')
-            c = str(a)[i+1:]
-            z.append(c)
-    return z
+        for r in res:
+            a = str(*r)
+            idx = str(a).find(' ')
+            c = str(a)[idx+1:]
+            multiplexed_results.append(c)
+
+    return multiplexed_results
 
 
-def chunk_data(d, cs):
-    ch = [d[x:x + cs] for x in range(0, len(d), cs)]
-    d = []
-    for c in ch:
-        d.append(c)
-    return d
+def chunk_data(data, chunk_size):
+    """ Break up lists into chunks of n and return as a single list of specified chunks """
+
+    chunks = [data[x:x + chunk_size] for x in range(0, len(data), chunk_size)]
+
+    data = []
+    for chunk in chunks:
+        data.append(chunk)
+
+    return data
 
 
 def unchunk_data(data):
-    nd = []
-    for d in data:
-        for x in d:
-            nd.append(x)
-    return nd
+    """ Un-chunk the data when and if required. Requires tag=False during spawn(). """
+
+    new_data = []
+    for dat in data:
+        for x in dat:
+            new_data.append(x)
+    return new_data
