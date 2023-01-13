1caf075d4049923a905fee9159abcce28120127b
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2023-01-13 11:06:10 +0000

    Add files via upload

--- examples/example_11.py
+++ examples/example_11.py
@@ -7,6 +7,11 @@ def PySubPlexed(_data):
     """ Provide something for PySubPlexed to compute... In one line.
     Data Structure again. Observe data structure when passing many chunks
     to PySubPlexed.
+    Observe I/O with small sample sizes when providing PySubPlexed with expressions
+    and data combinations to test if your required expression and data will always
+    provide the correct results and data structure.
+    Sampling is even more important if your expression are variable and or with
+    truly variable data.
     """
     return pysubplexed.spawn(int(len(_data)), _data, restrained=False, tag=True, sort=True)
 
@@ -15,7 +20,7 @@ print('Starting Program X: Using PySubPlexed to compute...')
 
 """ Create some data (n items of data in a list) """
 data = []
-for i in range(0, 1024):
+for i in range(0, 2048):
     _str = str(i) + '+1'
     data.append(_str)
 print('Data Structure:  ', data)
