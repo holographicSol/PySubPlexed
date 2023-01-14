4bf32ec2db92bbe33475c8f462f26b03db80ea29
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2023-01-14 15:48:41 +0000

    Add files via upload

---
+++ devopment_12/example_correct_application.py
@@ -0,0 +1,39 @@
+""" Written by Benjamin Jack Cullen
+Intention: Example program using PySubPlexed."""
+import pysubplexed
+
+
+def PySubPlexed(_data):
+
+    return pysubplexed.spawn(int(len(_data)), _data, restrained=False, allow_literals=False, tag=False, sort=True)
+
+
+def UsingPySubPlexed():
+    """ Having covered all previous examples, you should notice that PySubPlexed is extremely beneficial for some
+    things and not others in python.
+
+    PySubPlexed is perfect for spawning n subprocesses for example, handling them and waiting for results and sorting
+    results and keeping all the results in order while waiting for results to come back in. All in one line inside
+    a program that leverages PySubPlexed in this way.
+
+    PySubPlexed is not great or the best thing to use for normal math expressions for example, at least not yet. Such
+    expressions could be performed faster in other ways.
+
+    Example application:
+    For example 2 default pings in half the time or 8 in the time of 1 is how one should
+    think about applying PySubPlexed. Because all the handling is done and in one line
+    everything comes back into one variable in order and tagged if specified.
+
+    """
+
+    data = ['"subprocess.getoutput(\'powershell ping 8.8.8.8\')"',
+            '"subprocess.getoutput(\'powershell ping 9.9.9.9\')"']
+
+    chunks = pysubplexed.chunk_data(data, 4)
+    results = []
+    for chunk in chunks:
+        results.append(PySubPlexed(chunk))
+    print(results)
+
+
+UsingPySubPlexed()
